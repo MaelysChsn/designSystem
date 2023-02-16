4339fb1303ac7bda0faadfb16f7c0e977eb1a847
Maelys chassin <m.chassin@altelis.com>
2023-02-16 10:32:24 +0100

    [do] do responsive

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,32 +18,43 @@ function LoginForm({style}) {
             <div className={style.login_header}>
                 <h1>{sigin ? "Sign in" : "Sign up"}</h1>
                 <div className={style.register_container}>
-                    <h2> not signed up ?</h2>
+                    <h2> Not signed up ?</h2>
                     <button className={`${style.btn_header} ${style.btn_outline_secondary} ${style.btn}`}
                             onClick={() => setSigin(!sigin)}>{sigin ? "Register" : "Sign in"}</button>
                 </div>
             </div>
-            <form onSubmit={handleSubmit} className={style.form_container}>
-                <FormRow
-                    style={style}
-                    label="Email:"
-                    type="email"
-                    value={email}
-                    placeholder="Your email"
-                    onChange={(event) => setEmail(event.target.value)
+            <form onSubmit={handleSubmit} className={style.form_section}>
+                <div className={style.form_container}>
+                    <FormRow
+                        style={style}
+                        label="Email"
+                        type="email"
+                        value={email}
+                        placeholder="Your email"
+                        onChange={(event) => setEmail(event.target.value)
+                        }
+                        icon="Eye-closed.svg"
+                    />
+                    <FormRow
+                        style={style}
+                        label="Password"
+                        type="password"
+                        value={password}
+                        placeholder="Your password"
+                        onChange={(event) => setPassword(event.target.value)}
+                        icon="Eye-opened.svg"
+                    />
+                </div>
+                <div className={style.form_container}>
+                    <button className={`${style.btn_primary} ${style.btn}`} type="submit"> {sigin ? "Sign in" : "Register"}</button>
+                    {
+                        sigin ?
+                            <p className='underline'>Forget password ?</p>
+                            :
+                            null
                     }
-                    icon="Eye-closed.svg"
-                />
-                <FormRow
-                    style={style}
-                    label="Password:"
-                    type="password"
-                    value={password}
-                    placeholder="Your password"
-                    onChange={(event) => setPassword(event.target.value)}
-                    icon="Eye-opened.svg"
-                />
-                <button className={`${style.btn_primary} ${style.btn}`} type="submit"> {sigin ? "Sign in" : "Register"}</button>
+                </div>
+
             </form>
         </div>
     );
